1a8442c95f2d50b359edadade3075b5e79e51409
Teeshakul <teesha119@gmail.com>
2026-04-28 14:51:44 +0530

    [Teesha] added 237 delete node in a linked list solution

---
+++ 237-delete-node-linked-list.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class Solution {
+    public void deleteNode(ListNode node) {
+        ListNode prev = null;
+        while (node != null && node.next != null) {
+            node.val = node.next.val;
+            prev = node;
+            node = node.next;
+        }
+        if (node != null) {
+            if (prev != null) prev.next = null;
+            else node = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        ListNode head = null, tail = null;
+
+        for (int i = 0; i < n; i++) {
+            int val = sc.nextInt();
+            ListNode newNode = new ListNode(val);
+
+            if (head == null) {
+                head = tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+
+        int pos = sc.nextInt();
+
+        ListNode node = head;
+        for (int i = 0; i < pos; i++) {
+            if (node != null) node = node.next;
+        }
+
+        if (node != null) {
+            Solution obj = new Solution();
+            obj.deleteNode(node);
+        }
+
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+
+        sc.close();
+    }
+}
